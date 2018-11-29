e77d00966a8d9c8cb863da7a84bccd4e8a406735
Bahran Abraham <bahran@Bahrans-MacBook-Air.local>
2018-11-29 00:25:15 -0700

    update to process.env.PORT

--- dealer.js
+++ dealer.js
@@ -133,6 +133,6 @@ app.get('/order', (req, res) => {
   res.json(newDeck.deck);
 });
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
